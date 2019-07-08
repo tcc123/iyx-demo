c28a4c5b43ee4ca341299866a6f36e86d5aacd56
lijunchang <lijunchang@iyunxiao.com>
2019-07-08 17:22:59 +0800

    todo

--- WeChatProjects/todolist/pages/index/index.js
+++ WeChatProjects/todolist/pages/index/index.js
@@ -1,7 +1,6 @@
 //index.js
 //获取应用实例
-
-const app = getApp()
+const app = getApp() //todo 这里为什么要定义这个变量？
 
 Page({
   data: {
@@ -16,6 +15,7 @@ Page({
     wx.setStorageSync('todo_logs', this.data.logs)
   },
   onLoad: function () {
+    //todo 定义变量使用es6的关键字
     var todos = wx.getStorageSync('todo_list') // 调用 WX API 从本地缓存中获取数据
     if (todos) {
       var leftCount = todos.filter(function (item) {
@@ -27,6 +27,10 @@ Page({
     if (logs) {
       this.setData({ logs: logs })
     }
+    //todo 换成这种写法更好
+    // this.setData({
+    //   logs: wx.getStorageSync('todo_logs') || []
+    // })
   },
   inputChangeHandle: function (e) {
     this.setData({
@@ -39,6 +43,7 @@ Page({
       lists.push({ name: this.data.inputValue, completed: false })
       var logs = this.data.logs
       logs.push({ timestamp: new Date().toLocaleString(), action: '新增', name: this.data.inputValue })
+      //todo toLocaleString()不能使用，在手机上显示的时间格式不正确，小程序开发的实际效果以手机为准，模拟器只是参考。
       // console.log(lists)
       this.setData({
         inputValue: '',
@@ -49,7 +54,7 @@ Page({
     }
     this.save()
   },
-  toggleTodoHandle: function (e) {
+  toggleTodoHandle: function (e) { //todo 变量定义太多，名字重复，能否简化？
     var index = e.currentTarget.dataset.index
     var lists = this.data.lists
     lists[index].completed = !lists[index].completed
@@ -66,7 +71,7 @@ Page({
     })
     this.save()
   },
-  removeTodoHandle:function(e){
+  removeTodoHandle:function(e){ //todo 变量定义太多，名字重复，能否简化？
     var index = e.currentTarget.dataset.index
     var lists = this.data.lists
     var remove = lists.splice(index, 1)[0]
@@ -100,7 +105,7 @@ Page({
       lists: lists,
       leftCount: this.data.allCompleted ? 0 : lists.length,
       logs: logs,
-      allCompleted:this.data.allCompleted
+      allCompleted:this.data.allCompleted //todo 这里的赋值？
     })
     this.save()
   },
@@ -108,11 +113,11 @@ Page({
     var lists = this.data.lists
     var remains = []
     for (var i = 0; i < lists.length; i++) {
-      if(lists[i].completed===false){
+      if(lists[i].completed===false){ //todo 这样的判断可以简写为if(!lists[i].completed) {}
         remains.push(lists[i])
       }
     }
-    var logs = this.data.logs
+    var logs = this.data.logs //todo 用var定义了变量，名字都一样，存在隐患
     logs.push({
       timestamp: new Date().toLocaleString(),
       action: '清空',
@@ -122,3 +127,11 @@ Page({
     this.save()
   },
 })
+
+//todo 思考以下问题
+
+// 1、todolist应该有哪些功能？
+// 2、为什么删除默认模版代码？
+// 3、为什么建议使用es6写小程序？
+// 4、代码里面是否需要注释？注释应该加在什么地方？
+// 5、出现大量的重复定义变量，怎么办？
